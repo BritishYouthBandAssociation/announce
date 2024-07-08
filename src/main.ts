bbcf10b88bebd8172fac5f8acf595036ff959188
Luke Taylor <luke@circleleasing.co.uk>
2024-07-08 15:19:25 +0100

    Extract winners from CompSuite

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,81 @@ app.get("/", (_, res) => {
     });
 });
 
+type CompSuiteCategory = {
+    Captions: {
+        Name: string;
+        Rank: number;
+    }[]
+}
+
+type CompSuiteResult = {
+    Categories: CompSuiteCategory[];
+    DivisionName: string;
+    Rank: number;
+    GroupName: string;
+    TotalScore: number;
+}
+
+type BandResult = {
+    Name: string;
+    Rank: number;
+    Score: number;
+}
+
+type Result = {
+    Bands: BandResult[];
+    CaptionWinners: { [key: string]: string };
+}
+
+app.get("/:id", async (req, res) => {
+    const data: CompSuiteResult[] = await fetch(`https://api.competitionsuite.com/2018-03/performances?c=${req.params.id}`).then(res => res.json());
+    const divisions: Record<string, Result> = {};
+
+    if ('Message' in data) {
+        return res.redirect("/?error=Failed to find recap. Please make sure the link is correct!")
+    }
+
+    if (data.length == 0) {
+        return res.redirect("/?error=Empty recap. Have you allowed programmatic access?");
+    }
+
+    data.forEach(el => {
+        const bandResult: BandResult = {
+            Name: el.GroupName,
+            Rank: el.Rank,
+            Score: el.TotalScore
+        }
+
+        if (el.DivisionName in divisions) {
+            let inserted = false;
+
+            for(let i = 0; i < divisions[el.DivisionName].Bands.length; i++){
+                if(el.Rank < divisions[el.DivisionName].Bands[i].Rank){
+                    divisions[el.DivisionName].Bands.splice(i, 0, bandResult);
+                    inserted = true;
+                    break;
+                }
+            }
+
+            if(!inserted){
+                divisions[el.DivisionName].Bands.push(bandResult);
+            }
+        } else {
+            divisions[el.DivisionName] = { Bands: [bandResult], CaptionWinners: {} };
+        }
+
+        el.Categories.forEach(cat => {
+            cat.Captions.forEach(cap => {
+                if(cap.Rank == 1){
+                    divisions[el.DivisionName].CaptionWinners[cap.Name] = el.GroupName;
+                }
+            })
+        });
+    });
+
+    return res.json(divisions);
+});
+
 //final catch-all 404
 app.use((_, res) => res.status(404).render("error", {
     title: "Not Found"
