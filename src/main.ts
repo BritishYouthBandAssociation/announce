e7aa3641ed5fae0844fa1aba2840ff8360599def
Luke Taylor <luke@circleleasing.co.uk>
2024-07-08 14:04:26 +0100

    Tart up page design

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,20 @@
 import express from 'express';
 import 'dotenv/config';
-import {engine} from 'express-handlebars';
+import { engine } from 'express-handlebars';
 
 const app = express();
 
-app.engine("hbs", engine({extname: ".hbs"}));
+app.engine("hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
-
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
     res.send("hi");
 });
 
 //final catch-all 404
-app.use((_, res) => res.status(404).render("error"));
+app.use((_, res) => res.status(404).render("error", {
+    title: "Not Found"
+}));
 
 const port = parseInt(process.env.PORT ?? "0");
 app.listen(port, () => `Listening on port ${port}`);
